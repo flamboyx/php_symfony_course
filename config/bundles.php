9c95cca464aac64d2878f20bbeab32a154e955db
Timofey Trepachenko <tretim2014@gmail.com>
2024-11-12 05:27:53 +1000

    added Project fixture

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
